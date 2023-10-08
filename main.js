a294ae468db33f21215e844a4eb60db084b8b510
MohammadALShboul99 <110593677+MohammadALShboul99@users.noreply.github.com>
2023-10-08 13:31:40 +0300

    solve the lab

---
+++ main.js
@@ -0,0 +1,103 @@
+/* START CODE UNDER THIS LINE */
+
+//Qus 1
+
+//9 + 1 * 5=14
+//9%2=1
+//(9 / 3) * (10 - 7)=9
+// 21 / 3 + 3 * 9 * 9 + 5=255
+// 36 / 9 + 48 - 10 / 2=47
+
+
+//  Qus 2
+// const @name = 'John'; 
+const $name='John'
+// const 'age' = 10;
+const  age=10
+//const const = "Constant";
+const _const = "Constant";
+// const variable 1 = "value";
+const variableOne=5
+
+// Qus 3
+// Define a variable color containing your favorite color.
+let color ="blue"
+// Define a variable positiveNumber containing any positive number.
+let positiveNumber=600
+// Define a variable food containing your favorite food.
+let food="mansaf"
+// Define a variable PhoneNumber containing a phone number.
+let phoneNumber=+962773431123
+
+// Qus 4
+10 + "10"; // '1010'
+"10" + "10"; // '1010'
+10 + 10 + "5"; // '205'
+"Hello" + 1; // 'Hello1'
+"10" + 5 + 5; // '1010'
+"20" - 10; // 10
+"9" - "5"; // 4
+"19" - "13" + 17; // 23
+5 + 6 + "4" + 9 - 4 - 2; // 1143
+
+// Qus 5
+let fName='mohammad '
+let lName='shboul'
+let gender="male"
+let nationality="jordanian"
+console.log(`My Name is ${fName+lName} and nationality ${nationality} and i am a ${gender}`)
+
+// Qus 6
+const hoursInDay = 24
+const numberOfMinetsInHour = 60
+const numberOfSecoundInMinets = 60
+let numberOfDays = 30
+console.log((hoursInDay*numberOfMinetsInHour*numberOfSecoundInMinets)*numberOfDays)
+
+//Qus 7
+// Reassign the value of a to make the following expression equal to nine.
+//a =29
+let a=29;
+let b = 3;
+let c = 10;
+
+console.log(c - b * c + a)
+
+
+//Qus 8
+//Using the variables below write an expression that calculates the total amount for buying two coffee cups.
+const tip = 0.1;
+const taxRate = 0.08;
+const coffeeCupPrice = 6;
+let numberOfCoffeeCup = 2
+console.log((coffeeCupPrice+taxRate+tip)*numberOfCoffeeCup)
+
+//Qus 9
+/*Write an expression that represents the average grade of an exam, knowing that in a class of ten students,
+ five of them got 24/30, two of them got 16/30 and the remainder got 29/30.*/
+ const gradeOne=24
+ const gradeTwo=16
+ const gradeThree=29
+ const numberOfStudents=10
+ console.log(((gradeOne*5)+(gradeTwo*2)+(gradeThree*3))/numberOfStudents)
+
+ //Qus 10
+ //Write expressions that calculate the area and the perimeter of a rectangle with the length of 10m and width of 5m.
+let length=10
+let width=5
+let area=length*width
+console.log(`${area}`)
+
+//Qus 11
+//Write expressions that calculate the diameter and the circumference of a circle with the radius of 5m.
+let _radius=5
+let _diameter=(2*_radius)
+const pi=3.14
+let circumFerence=pi*_diameter
+console.log(circumFerence)
+
+//Qus 12
+//Write an expression that converts 30 degrees celsius to fahrenheit.
+let celsius=30
+let fahrenheit=(1.8*celsius)+32
+console.log(fahrenheit)
